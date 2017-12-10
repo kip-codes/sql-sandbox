3a5ec2f95050f9590f531f4fbbf069ac808ee276
Kevin Ip - Macbook '13 <kevin.ip.j@gmail.com>
2017-12-09 22:04:42 -0800

    2.8; CASE conditional in ORDER BY

--- 2. Single-Table Queries/exercises.sql
+++ 2. Single-Table Queries/exercises.sql
@@ -11,6 +11,7 @@ SELECT
   empid
 FROM Sales.Orders
 WHERE MONTH(orderdate) = 6 AND YEAR(orderdate) = 2007;
+-- CORRECT
 
 
 /*
@@ -22,6 +23,7 @@ USE TSQL2012
 SELECT orderid, orderdate, custid, empid
 FROM Sales.Orders
 WHERE orderdate = EOMONTH(orderdate);
+-- CORRECT
 
 
 /*
@@ -34,6 +36,7 @@ USE TSQL2012
 SELECT empid, firstname, lastname
 FROM HR.Employees
 WHERE lastname LIKE N'%a%a%'
+-- CORRECT
 
 
 /*
@@ -58,3 +61,100 @@ FROM Sales.OrderDetails
 GROUP BY orderid
 HAVING SUM(qty*unitprice) > 10000  -- Checks, by orderid, which totalvalues > 10,000
 ORDER BY totalvalue DESC;
+-- CORRECT
+
+
+/*
+5. Write a query against the Sale.Orders table that returns the three shipped-to countries with the highest average
+freight in 2007.
+-- Tables involved: TSQL2012 database and the Sales.Orders table
+ */
+
+USE TSQL2012
+SELECT TOP(3)
+  shipcountry,
+  SUM(freight) / COUNT(*) AS avgfreight
+FROM Sales.Orders
+WHERE YEAR(orderdate) = 2007
+GROUP BY shipcountry
+ORDER BY avgfreight DESC;
+-- CORRECT
+
+
+/*
+6. Write a query against the Sales.Orders table that calculates row numbers for orders based on order date ordering
+(using the order ID as the tiebreaker) for each customer separately.
+-- Tables involved: TSQL2012 database and the Sales.Orders table
+ */
+
+USE TSQL2012
+SELECT
+  custid,
+  orderdate,
+  orderid,
+  ROW_NUMBER() OVER(PARTITION BY custid
+                    ORDER BY orderdate, orderid) AS rownum
+FROM Sales.Orders
+-- GROUP BY orderdate, custid, orderid -- Grouping is not needed
+ORDER BY custid, rownum;
+-- CORRECT
+
+
+/*
+7. Using the HR.Employees table, figure out the SELECT statement that returns for each employee the gender based on the
+title of courtesy. For 'Ms.' and 'Mrs.' return 'Female'; for 'Mr.' return 'Male'; and in all other cases (for example,
+'Dr.') return 'Unknown'.
+-- Tables involved: TSQL2012 database and the HR.Employees table
+ */
+
+USE TSQL2012
+SELECT
+  empid,
+  firstname,
+  lastname,
+  titleofcourtesy,
+  CASE(titleofcourtesy)
+    WHEN 'Mrs.' THEN 'Female'
+    WHEN 'Ms.' THEN 'Female'
+    WHEN 'Mr.' THEN 'Male'
+    ELSE 'Unknown'
+  END AS gender
+FROM HR.Employees
+-- CORRECT
+
+-- Alernative solution
+USE TSQL2012
+SELECT
+  empid,
+  firstname,
+  lastname,
+  titleofcourtesy,
+  CASE
+    WHEN titleofcourtesy IN('Mrs.', 'Ms.') THEN 'Female'
+    WHEN titleofcourtesy = 'Mr.' THEN 'Male'
+    ELSE 'Unknown'
+  END AS gender
+FROM HR.Employees
+-- CORRECT
+
+
+/*
+8. Write a query against the Sales.Customers table that returns for each customer the customer ID and region.
+Sort the rows in the output by region, having NULL marks sort last (after non-NULL values).
+Note that the default sort behavior for NULL marks in T-SQL is to sort first (before non-NULL values).
+-- Tables involved: TSQL2012 database and the Sales.Customers table.
+ */
+
+USE TSQL2012
+SELECT
+  custid,
+  region
+FROM Sales.Customers
+GROUP BY region, custid
+ORDER BY
+  CASE
+    WHEN region IS NULL THEN 1
+    ELSE 0
+  END, region;
+
+-- Turns out, GROUP BY is not necessary because there was no aggregate function in the SELECT clause.
