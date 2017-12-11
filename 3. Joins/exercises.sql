d8d40eabb07b0a51903617d5e828c6e8f13e5a34
Kevin Ip - Macbook '13 <kevin.ip.j@gmail.com>
2017-12-11 04:11:33 -0800

    Added HTML tables of database files

--- 3. Joins/exercises.sql
+++ 3. Joins/exercises.sql
@@ -25,3 +25,41 @@ FROM HR.Employees
 ORDER BY empid;
 -- CORRECT
 -- Solution uses WHERE with a CROSS JOIN instead of JOIN ON, but gives same results
+
+
+/*
+2. Return United States customers, and for each customer return the total number of orders and total quantities.
+-- Tables involved: Sales.Customers, Sales.ORders, and Sales.OrderDetails
+ */
+
+USE TSQL2012;
+SELECT c.custid, COUNT(DISTINCT o.orderid) as numorders, SUM(od.qty) as totalqty
+FROM Sales.Customers as c
+  JOIN Sales.Orders as o
+    ON c.custid = o.custid
+  JOIN Sales.OrderDetails as od
+    ON o.orderid = od.orderid
+WHERE c.country = 'USA'
+GROUP BY c.custid
+ORDER BY c.custid;
+-- CORRECT
+-- NEED TO REVIEW DISTINCT
+-- DISTINCT prevents double counting from the orders --> orderdetails join.
+
+
+/*
+3. Return customers and their orders, including customers who placed no orders.
+-- Tables involved: Sales.Customers and Sales.Orders
+ */
+
+USE TSQL2012;
+SELECT c.custid, c.companyname, o.orderid, o.orderdate
+FROM Sales.Customers AS c
+  LEFT JOIN Sales.Orders AS o
+    ON c.custid = o.custid
+-- CORRECT
+
+
+/*
+4.
+ */
