6ae1b380e4fc1b2ae0259cf3b38c70b1b7123a0a
Kevin Ip - Macbook '13 <kevin.ip.j@gmail.com>
2017-12-22 15:55:33 -0800

    finished ch. 4 exercises, subqueries

--- 4. Subqueries/exercises.sql
+++ 4. Subqueries/exercises.sql
@@ -82,7 +82,6 @@ SELECT MAX(orderdate)
 FROM Sales.Orders
 -- WHERE custid matches the row we're analyzing
 
-
 SELECT DISTINCT o1.custid, o1.orderid, o1.orderdate, o1.empid
 FROM Sales.Orders o1
 WHERE o1.orderdate =
@@ -90,4 +89,106 @@ WHERE o1.orderdate =
       FROM Sales.Orders o2  -- Make a copy of the same table to make self-comparisons.
       WHERE o2.custid = o1.custid)
 ORDER BY o1.custid
+-- CORRECT
+
+
+/*
+6. Write a query that returns customers who placed orders in 2007 but not in 2008.
+-- Tables involved: Sales.Customers and Sales.Orders
+ */
+
+-- Get orders placed in 2008 ONLY
+SELECT *
+FROM Sales.Orders
+WHERE YEAR(orderdate) = 2008
+
+-- Get customers that placed orders in 2008 ONLY
+SELECT Customers.custid
+FROM Sales.Customers
+  JOIN Sales.Orders
+    ON Customers.custid = Orders.custid
+WHERE YEAR(Orders.orderdate) = 2008
+
+SELECT C.custid, C.companyname
+FROM Sales.Customers C
+   JOIN Sales.Orders O
+    ON C.custid = O.custid
+    AND YEAR(O.orderdate) = 2007
+WHERE C.custid NOT IN
+      (SELECT Customers.custid
+      FROM Sales.Customers
+        JOIN Sales.Orders
+          ON Customers.custid = Orders.custid
+      WHERE YEAR(Orders.orderdate) = 2008)
+GROUP BY C.custid, C.companyname
+-- CORRECT
+
+-- SOLUTION FROM THE BOOK
+SELECT custid, companyname
+FROM Sales.Customers C
+WHERE EXISTS
+      (SELECT *
+      FROM Sales.Orders O
+      WHERE O.custid = C.custid
+        AND O.orderdate >= '20070101'
+        AND O.orderdate < '20080101')
+      AND NOT EXISTS
+      (SELECT *
+      FROM Sales.Orders O
+      WHERE O.custid = C.custid
+        AND O.orderdate >= '20080101'
+        AND O.orderdate < '20090101')
+;
+-- RUNTIME IS APPROXIMATELY THE SAME
+
+
+/*
+7. Write a query that returns customers who ordered product 12.
+Tables involved: Sales.Customers, Sales.Orders, and Sales.OrderDetails
+ */
+
+-- Get orders with pid 12
+SELECT orderid
+FROM Sales.OrderDetails
+WHERE productid = 12
+
+-- Get cid based on oid
+SELECT custid
+FROM Sales.Orders
+WHERE orderid IN
+      (SELECT orderid
+      FROM Sales.OrderDetails
+      WHERE productid = 12)
+
+-- Get customers based on cid
+SELECT custid, companyname
+FROM Sales.Customers
+WHERE custid IN
+      (SELECT custid
+      FROM Sales.Orders
+      WHERE orderid IN
+            (SELECT orderid
+            FROM Sales.OrderDetails
+            WHERE productid = 12))
+ORDER BY companyname
+-- CORRECT!
+
+
+/*
+8. Write a query that calculates a running-total quantity for each customer and month.
+-- Tables involved: Sales.CustOrders
+ */
+
+-- Get the running quantity
+SELECT SUM(o2.qty) as runqty
+FROM Sales.CustOrders as o2
+GROUP BY o2.custid, o2.ordermonth
+ORDER BY o2.custid
+
+SELECT custid, ordermonth, qty,
+  (SELECT SUM(o1.qty)
+  FROM Sales.CustOrders as o1
+  WHERE o1.custid = o2.custid AND o1.ordermonth <= o2.ordermonth) as runqty
+FROM Sales.CustOrders as o2
+ORDER BY o2.custid
 -- CORRECT
