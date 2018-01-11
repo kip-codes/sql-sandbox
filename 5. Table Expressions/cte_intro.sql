7dff9c9b3cfed4e445cba5b65a47110f4e7ba512
Kevin Ip - Macbook '13 <kevin.ip.j@gmail.com>
2018-01-10 20:09:38 -0800

    intro to common table expressions

---
+++ 5. Table Expressions/cte_intro.sql
@@ -0,0 +1,9 @@
+USE TSQL2012;
+
+/*
+A query must meet three requirements to be valid to define a table expression of any kind:
+1. Order is not guaranteed.
+2. All column must have names.
+3. All column names must be unique.
+ */
+
