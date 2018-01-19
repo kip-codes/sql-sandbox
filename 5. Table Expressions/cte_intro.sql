5cea5ac7635eaa50c67dde3271aceaac903d46b0
Kevin Ip - Macbook '13 <kevin.ip.j@gmail.com>
2018-01-19 15:22:07 -0800

    intro to scalar vars

--- 5. Table Expressions/cte_intro.sql
+++ 5. Table Expressions/cte_intro.sql
@@ -7,3 +7,12 @@ A query must meet three requirements to be valid to define a table expression of
 3. All column names must be unique.
  */
 
+-- USING ARGUMENTS
+
+DECLARE @empid AS INT = 3;  -- Need to execute in the same process as where the variable is implemented
+SELECT orderyear, COUNT(DISTINCT custid) AS numcusts
+FROM (SELECT YEAR(orderdate) AS orderyear, custid
+  FROM Sales.Orders
+  WHERE empid = @empid) AS D
+GROUP BY orderyear;
+
